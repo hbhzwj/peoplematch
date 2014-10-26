11d9299500501845905eca8b626a48de13bf8739
Jing Conan Wang <hbhzwj@gmail.com>
2014-10-25 22:00:29 -0700

    add people match code. input user_ids, return a list of matched people

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask.ext import restful
+from match import Matcher
+import json
 
 app = Flask(__name__)
 api = restful.Api(app)
@@ -8,11 +10,18 @@ todos = {
     'todo1': 'remember your milk',
 }
 class TodoSimple(restful.Resource):
-    def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
+    def get(self, user_id):
+        # return {todo_id: todos[todo_id]}
+        # return 'good'
+        mc = Matcher()
+        try:
+            user_id = int(user_id)
+        except:
+            return "you have entered an invalid url"
+        return json.dumps({'matched_ids':mc.get_matched_persons(user_id, 2)})
 
 # api.add_resource(HelloWorld, '/')
-api.add_resource(TodoSimple, '/<string:todo_id>')
+api.add_resource(TodoSimple, '/<string:user_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
